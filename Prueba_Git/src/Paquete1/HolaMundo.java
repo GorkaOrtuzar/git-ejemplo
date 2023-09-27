c3983025260db7eebb81b4ec2775036a1d04859d
GorkaOrtuzar <145966852+GorkaOrtuzar@users.noreply.github.com>
2023-09-27 10:37:21 +0200

    añadido suma HolaMundo.java

--- Prueba_Git/src/Paquete1/HolaMundo.java
+++ Prueba_Git/src/Paquete1/HolaMundo.java
@@ -3,6 +3,8 @@ package Paquete1;
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!!!");
-	
+	public int sumar(int num1, int num2);
+		num1+num2;
+			
 }
 }
